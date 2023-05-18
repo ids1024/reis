2400102d668fb48d7c00502eca89453339d1646d
Ian Douglas Scott <idscott@system76.com>
2023-05-18 14:43:07 -0700

    Add script for updating protocol bindings

---
+++ update-protocol-bindings.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+	>&2 echo "Usage: update-protocol-bindings.sh [path/to/libei]"
+	exit 1
+fi
+
+"$1/proto/ei-scanner" --component=eis --output=src/eiproto_eis.rs "$1/proto/protocol.xml" src/eiproto.rs.tmpl
+"$1/proto/ei-scanner" --component=ei --output=src/eiproto_ei.rs "$1/proto/protocol.xml" src/eiproto.rs.tmpl
+rustfmt src/eiproto_eis.rs
+rustfmt src/eiproto_ei.rs
