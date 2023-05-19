02338deb3c7efa7c54e2df39e2a91ba70ad27154
Ian Douglas Scott <idscott@system76.com>
2023-05-19 14:35:09 -0700

    serial, protocol error

--- examples/reis-demo-server.rs
+++ examples/reis-demo-server.rs
@@ -2,6 +2,7 @@ use calloop::generic::Generic;
 use reis::eis;
 use std::{
     collections::VecDeque,
+    io,
     os::unix::io::{AsRawFd, OwnedFd, RawFd},
 };
 
@@ -10,6 +11,32 @@ struct ConnectionState {
     read_buffer: Vec<u8>,
     read_fds: Vec<OwnedFd>,
     handshake: eis::handshake::Handshake,
+    connection_obj: Option<eis::connection::Connection>,
+    last_serial: u32,
+    name: Option<String>,
+    context_type: Option<eis::handshake::ContextType>,
+}
+
+impl ConnectionState {
+    fn next_serial(&mut self) -> u32 {
+        self.last_serial += 1;
+        self.last_serial
+    }
+
+    fn disconnected(
+        &self,
+        reason: eis::connection::DisconnectReason,
+        explaination: &str,
+    ) -> io::Result<calloop::PostAction> {
+        if let Some(connection) = self.connection_obj.as_ref() {
+            connection.disconnected(self.last_serial, reason, explaination);
+        }
+        Ok(calloop::PostAction::Remove)
+    }
+
+    fn protocol_error(&self, explanation: &str) -> io::Result<calloop::PostAction> {
+        self.disconnected(eis::connection::DisconnectReason::Protocol, explanation)
+    }
 }
 
 impl AsRawFd for ConnectionState {
@@ -39,12 +66,17 @@ fn main() {
                 handshake.handshake_version(1);
                 handshake.interface_version("ei_callback", 1);
                 handshake.interface_version("ei_connection", 1);
+                handshake.interface_version("ei_seat", 1);
 
                 let connection_state = ConnectionState {
                     connection,
                     read_buffer: Vec::new(),
                     read_fds: Vec::new(),
                     handshake,
+                    connection_obj: None,
+                    last_serial: 0,
+                    name: None,
+                    context_type: None,
                 };
                 let source = Generic::new(
                     connection_state,
@@ -59,8 +91,7 @@ fn main() {
                             .connection
                             .recv(&mut buf, &mut connection_state.read_fds)
                             .unwrap();
-                        if count == 0 {
-                            // TODO handle any messages first
+                        if count == 0 && connection_state.read_buffer.len() < 16 {
                             return Ok(calloop::PostAction::Remove);
                         }
                         connection_state
@@ -76,9 +107,8 @@ fn main() {
                             return Ok(calloop::PostAction::Continue);
                         }
 
-                        // XXX protocol error
                         if header.length < 16 {
-                            return Ok(calloop::PostAction::Remove);
+                            return connection_state.protocol_error("header length < 16");
                         }
 
                         if let Some(interface) = connection_state
@@ -94,22 +124,46 @@ fn main() {
                             println!("{:?}", request);
                             match request {
                                 Some(eis::Request::Handshake(request)) => match request {
-                                    eis::handshake::Request::ContextType { context_type } => {}
+                                    eis::handshake::Request::ContextType { context_type } => {
+                                        if connection_state.context_type.is_some() {
+                                            return connection_state.protocol_error(
+                                                "context_type can only be sent once",
+                                            );
+                                        }
+                                        connection_state.context_type = Some(context_type);
+                                    }
+                                    eis::handshake::Request::Name { name } => {
+                                        if connection_state.name.is_some() {
+                                            return connection_state
+                                                .protocol_error("name can only be sent once");
+                                        }
+                                        connection_state.name = Some(name);
+                                    }
                                     eis::handshake::Request::Finish => {
-                                        // XXX serial
-                                        connection_state.handshake.connection(0, 1);
+                                        // May prompt user here whether to allow this
+                                        let serial = connection_state.next_serial();
+                                        connection_state.connection_obj = Some(
+                                            connection_state
+                                                .handshake
+                                                .connection(serial, 1)
+                                                .unwrap(),
+                                        );
                                     }
                                     _ => {}
                                 },
                                 Some(eis::Request::Connection(request)) => match request {
                                     eis::connection::Request::Disconnect => {
+                                        // Do not send `disconnected` in response
                                         return Ok(calloop::PostAction::Remove);
+                                    }
+                                    eis::connection::Request::Sync { callback } => {
+                                        callback.done(0);
                                     }
                                     _ => {}
                                 },
                                 None => {
-                                    // XXX protocol error
-                                    return Ok(calloop::PostAction::Remove);
+                                    return connection_state
+                                        .protocol_error("failed to parse request");
                                 }
                                 _ => {}
                             }
