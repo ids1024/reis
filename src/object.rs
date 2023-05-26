693c5f526d5fd791156df3fdd9ee1e0829b567c0
Ian Douglas Scott <idscott@system76.com>
2023-05-26 13:38:08 -0700

    Don't use `Arc` in `Object`

--- src/object.rs
+++ src/object.rs
@@ -2,30 +2,27 @@ use std::sync::Arc;
 
 use crate::{Arg, ConnectionInner, Interface};
 
-#[derive(Debug)]
-struct ObjectInner {
+#[derive(Clone, Debug)]
+pub struct Object {
     connection: Arc<ConnectionInner>,
     id: u64,
 }
 
-#[derive(Clone, Debug)]
-pub struct Object(Arc<ObjectInner>);
-
 impl Object {
     pub(crate) fn new(connection: Arc<ConnectionInner>, id: u64) -> Self {
-        Self(Arc::new(ObjectInner { connection, id }))
+        Self { connection, id }
     }
 
     pub fn connection(&self) -> &Arc<ConnectionInner> {
-        &self.0.connection
+        &self.connection
     }
 
     pub fn id(&self) -> u64 {
-        self.0.id
+        self.id
     }
 
     pub fn request(&self, opcode: u32, args: &[Arg]) -> rustix::io::Result<()> {
-        self.connection().request(self.id(), opcode, args)
+        self.connection.request(self.id, opcode, args)
     }
 
     pub(crate) fn downcast_unchecked<T: Interface>(self) -> T {
@@ -34,7 +31,7 @@ impl Object {
 
     // XXX test ei vs ei
     pub fn downcast<T: Interface>(self) -> Option<T> {
-        let (interface, _version) = self.connection().object_interface(self.id())?;
+        let (interface, _version) = self.connection.object_interface(self.id)?;
         if &interface == T::NAME {
             Some(T::downcast_unchecked(self))
         } else {
