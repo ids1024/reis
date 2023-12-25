c836d229245f19e46b42ed9a09cc446baf49f942
Ian Douglas Scott <idscott@system76.com>
2023-12-25 10:30:21 -0800

    Don't generate doc comments on private modules

--- src/eiproto_enum.rs
+++ src/eiproto_enum.rs
@@ -8,25 +8,7 @@
 
 // GENERATED FILE
 
-/**
-This is a special interface to setup the client as seen by the EIS
-implementation. The object for this interface has the fixed object
-id 0 and only exists until the connection has been set up, see the
-`ei_handshake.connection` event.
-
-The `ei_handshake` version is 1 until:
-- the EIS implementation sends the interface_version event with
-  a version other than 1, and, in response,
-- the client sends the interface_version request with a
-  version equal or lower to the EIS implementation version.
-
-The EIS implementation must send the interface_version event immediately
-once the physical connection has been established.
-
-Once the `ei_connection.connection` event has been sent the handshake
-is destroyed by the EIS implementation.
- */
-pub mod handshake {
+pub(crate) mod handshake {
     /**
     This enum denotes context types for the libei context.
 
@@ -73,14 +55,7 @@ pub mod handshake {
     }
 }
 
-/**
-The core connection object. This is the top-level object for any communication
-with the EIS implementation.
-
-Note that for a client to receive this object, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod connection {
+pub(crate) mod connection {
     /**
     A reason why a client was disconnected. This enum is intended to
     provide information to the client on whether it was disconnected as
@@ -151,59 +126,13 @@ pub mod connection {
     }
 }
 
-/**
-Interface for ensuring a roundtrip to the EIS implementation.
-Clients can handle the 'done' event to get notified when
-the related request that created the `ei_callback` object is done.
+pub(crate) mod callback {}
 
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod callback {}
+pub(crate) mod pingpong {}
 
-/**
-Interface for ensuring a roundtrip to the client implementation.
-This interface is identical to `ei_callback` but is intended for
-the EIS implementation to enforce a roundtrip to the client.
+pub(crate) mod seat {}
 
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod pingpong {}
-
-/**
-An `ei_seat` represents a set of input devices that logically belong together. In most
-cases only one seat is present and all input devices on that seat share the same
-pointer and keyboard focus.
-
-A seat has potential capabilities, a client is expected to bind to those capabilities.
-The EIS implementation then creates logical input devices based on the capabilities the
-client is interested in.
-
-Immediately after creation of the `ei_seat` object, the EIS implementation sends a burst
-of events with information about this seat. This burst of events is terminated by the
-`ei_seat.done` event.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod seat {}
-
-/**
-An `ei_device` represents a single logical input devices. Like physical input devices
-an `ei_device` may have multiple capabilities and may e.g. function as pointer
-and keyboard.
-
-Depending on the `ei_handshake.context_type`, an `ei_device` can
-emulate events via client requests or receive events. It is a protocol violation
-to emulate certain events on a receiver device, or for the EIS implementation
-to send certain events to the device. See the individual request/event documentation
-for details.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod device {
+pub(crate) mod device {
     /**
     If the device type is `ei_device.device_type.virtual`, the device is a
     virtual device representing input as applied on the EIS implementation's
@@ -258,61 +187,13 @@ pub mod device {
     }
 }
 
-/**
-Interface for pointer motion requests and events. This interface
-is available on devices with the `ei_device.capability` pointer.
+pub(crate) mod pointer {}
 
-This interface is only provided once per device and where a client
-requests `ei_pointer.release` the interface does not get re-initialized. An
-EIS implementation may adjust the behavior of the device (including removing
-the device) if the interface is releasd.
+pub(crate) mod pointer_absolute {}
 
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod pointer {}
+pub(crate) mod scroll {}
 
-/**
-Interface for absolute pointer requests and events. This interface
-is available on devices with the `ei_device.capability` pointer_absolute.
-
-This interface is only provided once per device and where a client
-requests `ei_pointer_absolute.release` the interface does not get
-re-initialized. An EIS implementation may adjust the behavior of the
-device (including removing the device) if the interface is releasd.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod pointer_absolute {}
-
-/**
-Interface for scroll requests and events. This interface
-is available on devices with the `ei_device.capability` scroll.
-
-This interface is only provided once per device and where a client
-requests `ei_scroll.release` the interface does not get
-re-initialized. An EIS implementation may adjust the behavior of the
-device (including removing the device) if the interface is releasd.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod scroll {}
-
-/**
-Interface for button requests and events. This interface
-is available on devices with the `ei_device.capability` button.
-
-This interface is only provided once per device and where a client
-requests `ei_button.release` the interface does not get
-re-initialized. An EIS implementation may adjust the behavior of the
-device (including removing the device) if the interface is releasd.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod button {
+pub(crate) mod button {
     /**
     The logical state of a button.
      */
@@ -355,19 +236,7 @@ pub mod button {
     }
 }
 
-/**
-Interface for keyboard requests and events. This interface
-is available on devices with the `ei_device.capability` keyboard.
-
-This interface is only provided once per device and where a client
-requests `ei_keyboard.release` the interface does not get re-initialized. An
-EIS implementation may adjust the behavior of the device (including removing
-the device) if the interface is releasd.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod keyboard {
+pub(crate) mod keyboard {
     /**
     The logical state of a key.
      */
@@ -447,16 +316,4 @@ pub mod keyboard {
     }
 }
 
-/**
-Interface for touchscreen requests and events. This interface
-is available on devices with the `ei_device.capability` touchscreen.
-
-This interface is only provided once per device and where a client
-requests `ei_touchscreen.release` the interface does not get re-initialized. An
-EIS implementation may adjust the behavior of the device (including removing
-the device) if the interface is releasd.
-
-Note that for a client to receive objects of this type, it must announce
-support for this interface in `ei_handshake.interface_version.`
- */
-pub mod touchscreen {}
+pub(crate) mod touchscreen {}
